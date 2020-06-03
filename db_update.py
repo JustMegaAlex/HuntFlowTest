ef8f6bbba12f3b06173b4f4efc8347a8fd00e31c
Alex <alex_govr@mail.ru>
2020-06-03 11:07:43 +0300

    add arg_parser object, assign arguments

--- db_update.py
+++ db_update.py
@@ -1,5 +1,12 @@
+import sys
 import requests
 import openpyxl
 import argparse
 
 API_ENDPOINT = 'https://dev-100-api.huntflow.ru'
+
+# handle external arguments
+argparser = argparse.ArgumentParser()
+argparser.add_argument('--token', '-t', required = True)
+argparser.add_argument('--path', '-p', required = True)
+ARGS = argparser.parse_args()
