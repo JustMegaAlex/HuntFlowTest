e25ca94a0435f39d7626b74cb51371e86327dc74
Alex <alex_govr@mail.ru>
2020-06-04 09:59:03 +0300

    upd API.add_candidate

--- db_update.py
+++ db_update.py
@@ -1,12 +1,9 @@
 import sys
 import os
-import requests
 import openpyxl
 import argparse
 import re
-import shutil
-import filetype
-import mimetypes
+from api import API
 
 # handle external arguments
 argparser = argparse.ArgumentParser()
@@ -20,18 +17,16 @@ except:
         token = f.read()
     ARGS = argparser.parse_args(rf'--path C:\repos\HuntFlowTest\test -t {token}'.split())
 
-UPLOAD_HEADERS = {
-    'X-File-Parse': 'true'
-}
+
 
 STATUSES_MAPPING = {
     'Отправлено письмо': 'Contacted',
-    'Интервью с HR': 'HR interview',
+    'Интервью с HR': 'HR Interview',
     'Выставлен оффер': 'Offered',
     'Отказ': 'Declined'
 }
 
-API_ENDPOINT = 'https://dev-100-api.huntflow.ru'
+
 
 DBFILE = 'Тестовая база.xlsx'
 
@@ -40,79 +35,6 @@ api = API(ARGS.token)
 STATUSES_IDS_MAPPING = api.get_statuses_ids_mapping()
 
 VACANCIES_IDS_MAPPING = api.get_vacancies_ids_mapping()
-
-class API:
-
-    def __init__(self, token):
-
-        self.authtoken = token
-        self.headers = {'Authorization':f'Bearer {self.authtoken}'}
-        self.account_id = 6
-
-    def get_statuses_ids_mapping(self):
-
-        statuses_ids = {}
-
-        statuses = self.send(api_method = 'vacancy/statuses')['items']
-
-        for item in statuses:
-
-            statuses_ids.update({item['name']:item['id']})
-
-        return statuses_ids
-
-    def get_vacancies_ids_mapping(self):
-
-        vacancies_ids = {}
-
-        vacancies = self.send(api_method = 'vacancy/statuses')['items']
-
-        for item in statuses:
-
-            vacancies_ids.update({item['position']:item['id']})
-
-        return vacancies_ids
-
-    def send(self, api_method, method = 'get', extraheaders = None, files = None):
-
-        if method == 'get':
-            method = requests.get
-        elif method == 'post':
-            method = requests.post
-        elif method == 'put':
-            method = requests.put
-
-        headers = self.headers.copy()
-
-        if extraheaders:
-
-            for h in extraheaders:
-                
-                headers.update(h)
-
-        r = method(f'{API_ENDPOINT}/account/{self.account_id}/{api_method}', headers = headers, files = files)
-
-        r.raise_for_status()
-
-        return r.json()
-
-    def add_candidate(self, candidate):
-
-        file_path = candidate['local_file']
-
-        data = {}
-
-    def upload_resume(self, file_path):
-
-        file_name = os.path.split(file_path)[1]
-
-        file_type = mimetypes.guess_type(file_path)[0]
-
-        with open(file_path, 'rb') as f:
-
-            data = self.send('upload', method = 'post', extraheaders = [UPLOAD_HEADERS], files = {'file':(file_name, f, file_type)})
-
-        return data
 
 
 def load_candidates_data(path):
@@ -134,9 +56,10 @@ def load_candidates_data(path):
         status_api_name = STATUSES_MAPPING[status_name]
         name = ws.cell(row, field_names['name']).value
         name_list = name.split()
-        file_path = get_resume_local_path(path, name, cand_data['position'])
+        position = ws.cell(row, field_names['position']).value
+        file_path = get_resume_local_path(path, name, position)
 
-        cand_data['position'] = ws.cell(row, field_names['position']).value
+        cand_data['position'] = position
         cand_data['money'] = ws.cell(row, field_names['money']).value
         cand_data['comment'] = ws.cell(row, field_names['comment']).value
         cand_data['status'] = STATUSES_IDS_MAPPING[status_api_name]
@@ -176,7 +99,7 @@ def get_resume_local_path(db_path, name, position):
 def create_cand_db_data(src_data):
     
     cand_data = {
-        'last_name': src_data['last_name'],
+        'last_name': src_data['second_name'],
         'first_name': src_data['first_name'],
         'middle_name': src_data['middle_name'],
         'phone': src_data['phone'],
@@ -201,19 +124,20 @@ if __name__ == '__main__':
     for cand in data:
 
         data_from_file = api.upload_resume(cand['local_file'])
+
         fields = data_from_file.get('fields')
         birth_date = fields.get('birthdate')
         phones = fields.get('phones')
-        phone_num = phones[0]
+        phone_num = phones[0] if phones else None
 
         externals = [{
             'data': {
-                'body': data_from_file['text']
+                'body': data_from_file.get('text')
             },
             'auth_type': 'NATIVE',
             'files': [
                 {
-                    'id': data_from_file['id']
+                    'id': data_from_file.get('id')
                 }
             ],
             'account_source': None
@@ -221,11 +145,15 @@ if __name__ == '__main__':
 
         cand['resume_text'] = data_from_file.get('text')
         cand['email'] = fields.get('email')
-        cand['birthday_day'] = birth_date.get('day')
-        cand['birthday_month'] = birth_date.get('month')
-        cand['birthday_year'] = birth_date.get('year')
+        cand['birthday_day'] = birth_date.get('day') if birth_date else None
+        cand['birthday_month'] = birth_date.get('month') if birth_date else None
+        cand['birthday_year'] = birth_date.get('year') if birth_date else None
         cand['phone'] = phone_num
         cand['photo'] = data_from_file.get('photo').get('id')
         cand['externals'] = externals
 
         cand = create_cand_db_data(cand)
+
+        api_data = api.add_candidate(cand)
+
+        print(api_data)
